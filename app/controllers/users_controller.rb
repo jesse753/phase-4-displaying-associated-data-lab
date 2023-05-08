e52538a58ac3db966bb7d1a894df4298f4f8a5ed
jesse <jesse.kibaara@student.moringaschool.com>
2023-05-08 21:47:00 +0300

    controller codes added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.includes(:items).find(params[:id])
+        render json: @user.as_json(include: { items: { only: [:id, :name, :description, :price] } })
+      end
 end
